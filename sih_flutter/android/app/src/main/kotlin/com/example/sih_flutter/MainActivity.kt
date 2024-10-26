06fa6f6bc19bd9797510be1db2d214118fd80131
Chinmay-27 <chinmayraut27@gmail.com>
2024-10-26 22:42:14 +0530

    Test

--- sih_flutter/android/app/src/main/kotlin/com/example/sih_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sih_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- sih_flutter/lib/constant.dart
+++
@@ -1,4 +0,0 @@
-const MONGO_URL = "mongodb+srv://SIH_Hospital:SIH@cluster0.grhlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const COLLECTION_NAME = "users";
-
-
